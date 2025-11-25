7674f1350679bcc08a3aaa78fc53a7d266cebb15
Darko Krstev <d.krstev@propel.us>
2025-11-25 13:33:30 +0100

    Update surcharge types

--- src/enum/SurchargeType.ts
+++ src/enum/SurchargeType.ts
@@ -3,18 +3,8 @@
  Types of surcharges that can be applied to orders or products
  */
 export enum SurchargeType {
-  /** Percentage-based surcharge */
-  PERCENTAGE = "PERCENTAGE",
-  /** Fixed amount surcharge */
-  FIXED_AMOUNT = "FIXED_AMOUNT",
-  /** Shipping surcharge */
-  SHIPPING = "SHIPPING",
-  /** Handling fee surcharge */
-  HANDLING = "HANDLING",
-  /** Payment processing fee */
-  PAYMENT_FEE = "PAYMENT_FEE",
-  /** Environmental fee */
-  ENVIRONMENTAL = "ENVIRONMENTAL",
-  /** Service charge */
-  SERVICE = "SERVICE"
+  /** Flat fee surcharge */
+  FlatFee = "FlatFee",
+  /** Percentage amount surcharge */
+  Percentage = "Percentage"
 }
